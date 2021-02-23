f0472bce7c487ebb153aa8eab328077fe6da93f9
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-02-23 15:59:52 -0500

    Add extra functionality
    Real-time validation before submit.
    Better user feedback when validation fails.
    Validation to prevent scheduling conflict among activities.

--- js/script.js
+++ js/script.js
@@ -67,11 +67,11 @@ function validateCreditCardNum() {
     return isValidCreditCardNum;
 }
 
-function validateCCV() {
-    const isValidCCV = /^\d{3}$/.test(+cvvInput.value);
-    if (isValidCCV) validationPass(cvvInput);
+function validateCVV() {
+    const isValidCVV = /^\d{3}$/.test(+cvvInput.value);
+    if (isValidCVV) validationPass(cvvInput);
     else validationFail(cvvInput);
-    return isValidCCV;
+    return isValidCVV;
 }
 
 function validateZipCode() {
@@ -91,7 +91,31 @@ function validationFail(element) {
     element.parentElement.classList.add('not-valid');
     element.parentElement.classList.remove('valid');
     element.parentElement.lastElementChild.style.display = 'inherit';
+
+    /* In this form, each type="text" input has as parentElement a label, but all type="checkbox" inputs have as parentElement
+     a fieldset. With the conditional below, we only run code for inputs of type="text", excluding type="checkbox"*/
+
+    if (element.parentElement.tagName === 'LABEL') {
+        const hintElement = element.parentElement.lastElementChild;
+        if (element.value !== '') {
+            const regex = /([A-Za-z]+ ?[A-Za-z]*)(\W)/;
+            const key = element.parentElement.childNodes[0].textContent.replace(regex, '$1').trim();
+            const validationMsgObject = validationMsgObjects.find((obj) => obj[key] !== undefined);
+            hintElement.textContent = validationMsgObject[key];
+        } else {
+            hintElement.textContent = 'Field cannot be blank';
+        }
+    }
 }
+
+// Array containing custom validation fail messages for for when input field is not '' (empty string)
+const validationMsgObjects = [
+    {'Name': 'Special characters and extra spaces not allowed - middlename is optional'},
+    {'Email Address': 'Email address must be formatted correctly'},
+    {'Card Number': 'Credit card number must be between 13 - 16 digits'},
+    {'Zip Code': 'Zip Code must be 5 digits'},
+    {'CVV': 'CVV must be 3 digits'}
+];
 
 
 // START OF DOM MANIPULATION
@@ -130,8 +154,26 @@ designSelect.addEventListener('change', (e) => {
 // update activitiesCostParagraph according to checked checkboxes and disable/enable checkboxes if schedule conflicts exist
 activitiesFieldset.addEventListener('change', (e) => {
     const activityCost = +e.target.getAttribute('data-cost');
-    if (e.target.checked) totalCost += activityCost;
-    else totalCost -= activityCost;
+    const selectedActivitySchedule = e.target.nextElementSibling.nextElementSibling.textContent;
+    if (e.target.checked) {
+        totalCost += activityCost;
+        activitiesCheckboxes.forEach((checkbox) => {
+            const activitySchedule = checkbox.nextElementSibling.nextElementSibling.textContent;
+            if(!checkbox.checked && activitySchedule === selectedActivitySchedule) {
+                checkbox.disabled = true;
+                checkbox.parentElement.classList.add('disabled');
+            }
+        });
+    } else {
+        totalCost -= activityCost;
+        activitiesCheckboxes.forEach((checkbox) => {
+            const activitySchedule = checkbox.nextElementSibling.nextElementSibling.textContent;
+            if(!checkbox.checked && activitySchedule === selectedActivitySchedule) {
+                checkbox.disabled = false;
+                checkbox.parentElement.classList.remove('disabled');
+            }
+        });
+    }
     activitiesCostParagraph.innerHTML = `Total: $${totalCost}`;
 });
 
@@ -167,9 +209,8 @@ form.addEventListener('submit', (e) => {
     validateActivities() ? console.log('ok activitis'): console.log('select at least 1 activity');
     
     if (paymentOptionSelect.children[1].selected) {
-        console.log('cc selected');
         validateCreditCardNum() ? console.log('ok cardnum'): console.log('invalid cardnum');
-        validateCCV() ? console.log('ok ccv'): console.log('invalid ccv');
+        validateCVV() ? console.log('ok CVV'): console.log('invalid CVV');
         validateZipCode() ? console.log('ok zip'): console.log('invalid zip');
     }
 });
@@ -179,3 +220,10 @@ for (let checkbox of activitiesCheckboxes) {
     checkbox.addEventListener('focus', (e) => e.target.parentElement.classList.add('focus'));
     checkbox.addEventListener('blur', (e) => e.target.parentElement.classList.remove('focus'));
 }
+
+// real-time validation on required input fields of type text
+nameInput.addEventListener('change', validateName);
+emailInput.addEventListener('change', validateEmail);
+cardNumberInput.addEventListener('change', validateCreditCardNum);
+zipCodeInput.addEventListener('change', validateZipCode);
+cvvInput.addEventListener('change', validateCVV);
